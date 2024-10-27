aa5b70c0813cd5bb1286b9e28c122535094d0d7d
ChuongMon <126510948+ChuongMon@users.noreply.github.com>
2024-10-27 17:26:16 +0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -4,15 +4,11 @@ import cv2
 def disparity_to_depth(baseline, f, img):
     """This is used to compute the depth values from the disparity map"""
 
-    # Assumption image intensities are disparity values (x-x') 
-    depth_map = np.zeros((img.shape[0], img.shape[1]))
-    depth_array = np.zeros((img.shape[0], img.shape[1]))
+    
+    epsilon = 1e-6
+    img = img + epsilon
 
-    for i in range(depth_map.shape[0]):
-        for j in range(depth_map.shape[1]):
-            depth_map[i][j] = 1/img[i][j]
-            depth_array[i][j] = baseline*f/img[i][j]
-            # if math.isinf(depth_map[i][j]):
-            #     depth_map[i][j] = 1
+    depth_map = 1 / img
+    depth_array = (baseline * f) / img
 
     return depth_map, depth_array
