c0269c33216b38e7fd81a239cf3c3d93746db19f
ChuongMon <126510948+ChuongMon@users.noreply.github.com>
2024-10-27 10:48:29 +0700

    Update rectification.py

--- rectification.py
+++ rectification.py
@@ -2,45 +2,33 @@ import numpy as np
 import cv2
 
 def rectification(img1, img2, pts1, pts2, F):
-    """This function is used to rectify the images to make camera pose's parallel and thus make epiplines as horizontal.
-        Since camera distortion parameters are not given we will use cv2.stereoRectifyUncalibrated(), instead of stereoRectify().
-    """
+    """Rectify images using stereoRectifyUncalibrated to align epilines as horizontal without camera distortion parameters."""
 
-    # Stereo rectification
     h1, w1 = img1.shape
     h2, w2 = img2.shape
 
-    _, H1, H2 = cv2.stereoRectifyUncalibrated(np.float32(pts1), np.float32(pts2), F, imgSize=(w1, h1))
-    print("H1",H1)
-    print("H2",H2)
+    # Stereo rectification using uncalibrated method
+    valid, H1, H2 = cv2.stereoRectifyUncalibrated(np.float32(pts1), np.float32(pts2), F, imgSize=(w1, h1))
+    if not valid:
+        raise ValueError("Stereo rectification failed. Check the fundamental matrix and matching points.")
+    
+    # Apply homography matrices to the feature points
+    pts1_homogeneous = np.hstack((pts1, np.ones((pts1.shape[0], 1))))
+    pts2_homogeneous = np.hstack((pts2, np.ones((pts2.shape[0], 1))))
 
-    rectified_pts1 = np.zeros((pts1.shape), dtype=int)
-    rectified_pts2 = np.zeros((pts2.shape), dtype=int)
+    rectified_pts1 = (H1 @ pts1_homogeneous.T).T
+    rectified_pts2 = (H2 @ pts2_homogeneous.T).T
 
-    # Rectify the feature points
-    for i in range(pts1.shape[0]):
-        source1 = np.array([pts1[i][0], pts1[i][1], 1])
-        new_point1 = np.dot(H1, source1)
-        new_point1[0] = int(new_point1[0]/new_point1[2])
-        new_point1[1] = int(new_point1[1]/new_point1[2])
-        new_point1 = np.delete(new_point1, 2)
-        rectified_pts1[i] = new_point1
+    # Normalize points
+    rectified_pts1 /= rectified_pts1[:, 2].reshape(-1, 1)
+    rectified_pts2 /= rectified_pts2[:, 2].reshape(-1, 1)
 
-        source2 = np.array([pts2[i][0], pts2[i][1], 1])
-        new_point2 = np.dot(H2, source2)
-        new_point2[0] = int(new_point2[0]/new_point2[2])
-        new_point2[1] = int(new_point2[1]/new_point2[2])
-        new_point2 = np.delete(new_point2, 2)
-        rectified_pts2[i] = new_point2
+    # Remove homogeneous coordinate
+    rectified_pts1 = rectified_pts1[:, :2].astype(int)
+    rectified_pts2 = rectified_pts2[:, :2].astype(int)
 
-    # Rectify the images and save them
+    # Rectify the images
     img1_rectified = cv2.warpPerspective(img1, H1, (w1, h1))
     img2_rectified = cv2.warpPerspective(img2, H2, (w2, h2))
-    
-    cv2.imwrite("rectified_1.png", img1_rectified)
-    cv2.imwrite("rectified_2.png", img2_rectified)
-    
+
     return rectified_pts1, rectified_pts2, img1_rectified, img2_rectified
-
-
-   
