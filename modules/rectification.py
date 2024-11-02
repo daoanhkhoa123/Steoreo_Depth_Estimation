c4fe7035216803d72ff6955e6b610edc602f05f4
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2024-11-02 20:29:58 +0700

    Add files via upload

--- modules/rectification.py
+++ modules/rectification.py
@@ -7,34 +7,31 @@ import cv2
 
 def rectification(img1: MatLike, img2: MatLike, pts1: NDArray, pts2: NDArray, F: NDArray) -> tuple[NDArray, NDArray, MatLike, MatLike]:
     """Rectify images using stereoRectifyUncalibrated to align epilines as horizontal without camera distortion parameters."""
-
-    h1, w1 = img1.shape
-    h2, w2 = img2.shape
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
 
     # Stereo rectification using uncalibrated method
     valid, H1, H2 = cv2.stereoRectifyUncalibrated(
-        np.float32(pts1), np.float32(pts2), F, imgSize=(w1, h1))
-    if not valid:
-        raise ValueError(
-            "Stereo rectification failed. Check the fundamental matrix and matching points.")
+        pts1, pts2, F, imgSize=(w1, h1))
+    assert valid, "Stereo rectification failed. Check the fundamental matrix and matching points."
 
     # Apply homography matrices to the feature points
     pts1_homogeneous = np.hstack((pts1, np.ones((pts1.shape[0], 1))))
     pts2_homogeneous = np.hstack((pts2, np.ones((pts2.shape[0], 1))))
 
-    rectified_pts1 = (H1 @ pts1_homogeneous.T).T
-    rectified_pts2 = (H2 @ pts2_homogeneous.T).T
+    pts1_homogeneous = (H1 @ pts1_homogeneous.T).T
+    pts2_homogeneous = (H2 @ pts2_homogeneous.T).T
 
     # Normalize points
-    rectified_pts1 /= rectified_pts1[:, 2].reshape(-1, 1)
-    rectified_pts2 /= rectified_pts2[:, 2].reshape(-1, 1)
+    pts1_homogeneous /= pts1_homogeneous[:, 2].reshape(-1, 1)
+    pts2_homogeneous /= pts2_homogeneous[:, 2].reshape(-1, 1)
 
     # Remove homogeneous coordinate
-    rectified_pts1 = rectified_pts1[:, :2].astype(int)
-    rectified_pts2 = rectified_pts2[:, :2].astype(int)
+    pts1_homogeneous = pts1_homogeneous[:, :2].astype(int)
+    pts2_homogeneous = pts2_homogeneous[:, :2].astype(int)
 
     # Rectify the images
     img1_rectified = cv2.warpPerspective(img1, H1, (w1, h1))
     img2_rectified = cv2.warpPerspective(img2, H2, (w2, h2))
 
-    return rectified_pts1, rectified_pts2, img1_rectified, img2_rectified
+    return pts1_homogeneous, pts2_homogeneous, img1_rectified, img2_rectified
